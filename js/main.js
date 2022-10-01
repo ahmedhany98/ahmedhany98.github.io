b1d274ca3704ad09fe7d2b588a60e560d9a72fec
ahmedhany98 <ahmedhany988@gmail.com>
2022-10-01 15:55:38 +0200

    Move function to js file

--- js/main.js
+++ js/main.js
@@ -20,3 +20,14 @@ function myFunction() {
   }
   prevScrollpos = currentScrollPos;
   }
+
+  $(function () {
+    count = 0;
+    wordsArray = ["cool", "amazing", "super"];
+    setInterval(function () {
+      count++;
+      $("#word").fadeOut(500, function () {
+        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500);
+      });
+    }, 2000);
+  });
