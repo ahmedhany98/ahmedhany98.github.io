28984d8d5bc79ddaf974ba6b72ddfe0b8dd1157c
Ahmed Hany <ahmedhany988@gmail.com>
2023-11-01 03:22:23 +0200

    Phase 1 of my website refactor

--- js/main.js
+++ js/main.js
@@ -1,39 +1,10 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-/* Changes the active class in the navbar */
-$(".topnav a").click(function() {
-  $(".topnav a.active").removeClass("active");
-  $(this).addClass("active");
+$(function () {
+  count = 0;
+  wordsArray = ["Software Engineer.", "Web Developer.", "Backend Developer."];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(500, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500);
+    });
+  }, 2000);
 });
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("myTopnav").style.top = "0";
-    console.log(document.getElementById("myTopnav").style.top);
-  }
-  else {
-    document.getElementById("myTopnav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-  }
-
-  $(function () {
-    count = 0;
-    wordsArray = ["Software Engineer", "Web Developer"];
-    setInterval(function () {
-      count++;
-      $("#word").fadeOut(500, function () {
-        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500);
-      });
-    }, 2000);
-  });
